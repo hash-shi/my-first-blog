3748fc978c0a9b726a015249a30db7d4c44e9e57
hash-shi <hash-shi@tjs-net.co.jp>
2023-02-20 15:15:12 +0900

    20230220_hash-shi_02

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
 	# ルートなしはviews.post_listへ遷移
 	path('', views.post_list, name='post_list'),
+
+	path('blog', views.post_list, name='post_list'),
+	path('qrPrint', views.qrPrint, name='qrPrint'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
+
+	#
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+
 	# post_list.htmlをリダイレクトする(画面表示)
-	return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def qrPrint(request):
+	return render(request, 'qrPrint/qrPrint.html', {})
